efa906854adbb220358fc7a8928ae831bfcc573a
Jiaming <424366965@qq.com>
2020-03-03 21:40:53 +0800

    test_mvt OK!

--- test_mvt/main.py
+++ test_mvt/main.py
@@ -10,39 +10,54 @@ Moteur_Droit = DRV8833_V2 (DRV8833_AIN1, DRV8833_AIN2,DRV8833_Sleep_pin, 1, 500,
 Moteur_Gauche = DRV8833_V2 (DRV8833_BIN1, DRV8833_BIN2,DRV8833_Sleep_pin, 1, 500, 0, 1, MOTEUR_GAUCHE_Flag)
 
 def Avancer (consigne_rotation_roue) :
-    while (True):
-        # Commande du moteur droit
-        Moteur_Droit.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue)
-        # Commande du moteur gauche
-        Moteur_Gauche.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue)
+    # Commande du moteur droit
+    Moteur_Droit.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue)
+    # Commande du moteur gauche
+    Moteur_Gauche.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue)
 
 def Reculer (consigne_rotation_roue) :
-    while (True):
-        # Commande du moteur droit
-        Moteur_Droit.Cmde_moteur(SENS_ANTI_HORAIRE,consigne_rotation_roue)
-        # Commande du moteur gauche
-        Moteur_Gauche.Cmde_moteur(SENS_ANTI_HORAIRE,consigne_rotation_roue)
+    # Commande du moteur droit
+    Moteur_Droit.Cmde_moteur(SENS_ANTI_HORAIRE,consigne_rotation_roue)
+    # Commande du moteur gauche
+    Moteur_Gauche.Cmde_moteur(SENS_ANTI_HORAIRE,consigne_rotation_roue)
 
 def Pivoter_droite (consigne_rotation_roue) :
-    while (True):
-        # Commande du moteur droit
-        Moteur_Droit.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue/2)
-        # Commande du moteur gauche
-        Moteur_Gauche.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue)
+    # Commande du moteur droit
+    Moteur_Droit.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue/3)
+    # Commande du moteur gauche
+    Moteur_Gauche.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue)
 
 
 def Pivoter_gauche (consigne_rotation_roue) :
-    while (True):
-        # Commande du moteur droit
-        Moteur_Droit.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue)
-        # Commande du moteur gauche
-        Moteur_Gauche.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue/2)
+    # Commande du moteur droit
+    Moteur_Droit.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue)
+    # Commande du moteur gauche
+    Moteur_Gauche.Cmde_moteur(SENS_HORAIRE,consigne_rotation_roue/3)
+    print("pivoter gauche", consigne_rotation_roue/3)
 
-consigne_rotation_roue = 0.5
-Avancer(consigne_rotation_roue)
-time.sleep (1)
-Reculer(consigne_rotation_roue)
-time.sleep (1)
-Pivoter_gauche (consigne_rotation_roue)
-time.sleep (1)
-Pivoter_droite (consigne_rotation_roue)
+def Arret () :
+    Moteur_Droit.Arret_moteur ()
+    Moteur_Droit.Arret_moteur ()
+
+consigne_rotation_roue = 1
+while (True):
+    Arret ()
+    time.sleep(0.05)
+    print("Avancer")
+    Avancer(consigne_rotation_roue)
+    time.sleep (6)
+    Arret ()
+    time.sleep (0.5)
+    print("Reculer")
+    Reculer(consigne_rotation_roue)
+    time.sleep (5)
+    Arret ()
+    time.sleep (0.5)
+    print("pivoter_gauche")
+    Pivoter_gauche (consigne_rotation_roue)
+    time.sleep (5)
+    Arret ()
+    time.sleep (0.5)
+    print("Pivoter_droite")
+    Pivoter_droite (consigne_rotation_roue)
+    time.sleep (5)
