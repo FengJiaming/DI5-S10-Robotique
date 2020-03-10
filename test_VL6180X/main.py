e91be2c79b4b8979c2a05ee8b7683ae677367c73
SIGESI <songsixte@gmail.com>
2020-03-10 15:47:17 +0100

    test 2 capteurs

--- test_VL6180X/main.py
+++ test_VL6180X/main.py
@@ -2,7 +2,7 @@ from VL6180X import*
 
 # Ressource GPIo de la carte WiPy3.0 affectee au controle
 # du capteur VL6180X
-VL6180X_CE_Pin = 'P3'
+VL6180X_CE_Pin = 'P5' #'p3'
 # Adressage I2C des capteurs VL6180X : par defaut 0x29 soit 41
 VL6180X_I2C_adr_defaut = const(0x29)
 
@@ -12,6 +12,9 @@ VL6180X_GPIO_CE_Pin.value(1) # Activer le capteur de distance
 i2c = I2C(0, I2C.MASTER, baudrate = 400000)
 adr = i2c.scan()
 print ('Adresse peripherique I2C (1) :', adr)
+
+
+
 #Le capteur VL6180X peut maintenant être initialisé :
 capteur_d_l_VL6180X = VL6180X(VL6180X_I2C_adr_defaut, i2c)
 
@@ -27,5 +30,7 @@ while True :
     print ('Luminosite : %.1f' %(Luminosite))
     print ('----------------------------------------')
     Index +=1
+'''
     if Index > 20:
         break
+'''
